c4b10ae12c1e16fadcc58af2c2c40c53b9ecb773
wilymonkey <codeberg@rohit.top>
2026-04-10 09:42:04 +1000

    feat: button logic

--- assets/build-your-own-box.js
+++ assets/build-your-own-box.js
@@ -1,3 +1,7 @@
+// ===================================
+// GLOBAL DATA
+// ===================================
+
 let dataRaw = {
   addons: new Set(),
   cupcakes: new Map(),
@@ -6,17 +10,21 @@ let dataRaw = {
   totalPrice: 0,
 
   cupcakeLength() {
-    let length = 0;
-    for (const [_, amount] of this.cupcakes.entries()) {
-      length += amount;
-    }
-    return length;
+    return measureCupcakeLength(this.cupcakes);
   },
 
-  name() {
-    return "cupcake-box-" + this.maxCupcakes;
+  variant() {
+    return "cupcake-box-" + this.maxCupcakes; // Replace this with variant id.
   },
 };
+
+function measureCupcakeLength(cupcakes) {
+  let length = 0;
+  for (const [_, amount] of cupcakes.entries()) {
+    length += amount;
+  }
+  return length;
+}
 
 const handler = {
   set(obj, prop, value) {
@@ -37,6 +45,12 @@ function onPropertyChange(property, oldValue, newValue) {
       updateCupcakeCounters();
       rerenderCupcakePreview();
       saveToLocalStorage();
+      checkBoxIsValid();
+      const wasMax = measureCupcakeLength(oldValue) == data.maxCupcakes;
+      const isMax = measureCupcakeLength(newValue) == data.maxCupcakes;
+      if (isMax || wasMax) {
+        clickAllCakeBtns();
+      }
       break;
     case "maxCupcakes":
       cullCupcakes();
@@ -52,30 +66,96 @@ function onPropertyChange(property, oldValue, newValue) {
 
 const data = new Proxy(dataRaw, handler);
 
+// ===================================
+// SAVE TO LOCAL STORAGE
+// ===================================
+
 function saveToLocalStorage() {
   const dataToStore = {
     addons: Array.from(dataRaw.addons),
     cupcakes: Array.from(dataRaw.cupcakes.entries()),
   };
-  localStorage.setItem(data.name(), JSON.stringify(dataToStore));
+  localStorage.setItem(data.variant(), JSON.stringify(dataToStore));
 }
 
 function getFromLocalStorage() {
-  const stored = localStorage.getItem(data.name());
+  const stored = localStorage.getItem(data.variant());
   if (stored) {
     const dataToLoad = JSON.parse(stored);
     data.addons = new Set(dataToLoad.addons);
     data.cupcakes = new Map(dataToLoad.cupcakes);
+    recheckAddons();
+    cupcakeElements.forEach(function (element) {
+      element.click();
+    });
   }
 }
+
+// ===================================
+// PRICE
+// ===================================
+
+function recalculateTotalPrice() {
+  if (data.basePrice == 0) return readBoxLayout();
+
+  let total = data.basePrice;
+  data.addons.forEach((addonId) => {
+    const addon = document.querySelector(`[data-addon-id="${addonId}"]`);
+    total += parseInt(addon.getAttribute("data-addon-price"));
+  });
+  data.totalPrice = total;
+}
+
+function updateTotalView() {
+  const formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "AUD",
+  });
+  document.querySelector("#total-price").textContent = formatter.format(
+    data.totalPrice / 100,
+  );
+}
+
+// ===================================
+// ADDONS
+// ===================================
+
+const addonElements = document.querySelectorAll("[data-addon-id]");
+addonElements.forEach(function (element) {
+  element.addEventListener("change", function () {
+    const addonId = this.getAttribute("data-addon-id");
+    const currentAddons = new Set(data.addons);
+
+    if (this.checked) {
+      currentAddons.add(addonId);
+    } else {
+      currentAddons.delete(addonId);
+    }
+
+    data.addons = currentAddons;
+  });
+});
+// Needed to restore state from storage.
+function recheckAddons() {
+  data.addons.forEach(function (addonId) {
+    const addonElement = document.querySelector(`[data-addon-id="${addonId}"]`);
+    if (addonElement) {
+      addonElement.checked = true;
+    }
+  });
+}
+
+// ===================================
+// CUPCAKES
+// ===================================
 
 function updateCupcakeCounters() {
   const cupcakeCounter = document.querySelector("#cupcake-counter");
   cupcakeCounter.textContent = data.cupcakeLength();
-  cupcakeElements.forEach(function (e) {
-    const cupcakeId = e.getAttribute("data-cupcake-id");
+  cupcakeElements.forEach(function (element) {
+    const cupcakeId = element.getAttribute("data-cupcake-id");
     const count = data.cupcakes.get(cupcakeId) || 0;
-    const counter = e.querySelector('[data-role="count"]');
+    const counter = element.querySelector('[data-role="count"]');
     counter.textContent = count;
   });
 }
@@ -102,49 +182,10 @@ function cullCupcakes() {
   }
 }
 
-function recalculateTotalPrice() {
-  if (data.basePrice == 0) return readBoxLayout();
-
-  let total = data.basePrice;
-  data.addons.forEach((addonId) => {
-    const addon = document.querySelector(`[data-addon-id="${addonId}"]`);
-    total += parseInt(addon.getAttribute("data-addon-price"));
-  });
-  data.totalPrice = total;
-}
-
-function updateTotalView() {
-  const formatter = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "AUD",
-  });
-  document.querySelector("#total-price").textContent = formatter.format(
-    data.totalPrice / 100,
-  );
-}
-
-const addonElements = document.querySelectorAll("[data-addon-id]");
-addonElements.forEach(function (element) {
-  element.addEventListener("change", function () {
-    const addonId = this.getAttribute("data-addon-id");
-    const currentAddons = new Set(data.addons);
-
-    if (this.checked) {
-      currentAddons.add(addonId);
-    } else {
-      currentAddons.delete(addonId);
-    }
-
-    data.addons = currentAddons;
-  });
-});
-
 const cupcakeElements = document.querySelectorAll("[data-cupcake-id]");
 cupcakeElements.forEach(function (element) {
-  // Capture cupcake stock so that it doesn't change from underneath
-  // the user (if it even can).
-  const stock = parseInt(element.getAttribute("data-cupcake-stock"));
   const cupcakeId = element.getAttribute("data-cupcake-id");
+  const stock = parseInt(element.getAttribute("data-cupcake-stock"));
   const minusBtn = element.querySelector('[data-action="decrement"]');
   const plusBtn = element.querySelector('[data-action="increment"]');
   const counter = element.querySelector('[data-role="count"]');
@@ -163,15 +204,72 @@ cupcakeElements.forEach(function (element) {
       }
       data.cupcakes = currentCupcakes;
     }
-
     const count = data.cupcakes.get(cupcakeId) || 0;
     plusBtn.disabled =
       count >= stock || data.cupcakeLength() >= data.maxCupcakes;
     minusBtn.disabled = count < 1;
-
     counter.innerHTML = count;
   });
 });
+
+function clickAllCakeBtns() {
+  cupcakeElements.forEach(function (element) {
+    element.click();
+  });
+}
+
+// ===================================
+// ADD TO CART
+// ===================================
+
+const addToCartBtn = document.querySelector("#add-to-cart");
+addToCartBtn.addEventListener("click", async function () {
+  const dataToCart = {
+    id: data.variant(),
+    quantity: 1,
+    properties: new Map(),
+  };
+  for (const [id, value] of data.cupcakes.entries()) {
+    const name = document.querySelector(
+      `[data-cupcake-name="${id}"]`,
+    ).innerText;
+    dataToCart.properties.set(name, value);
+  }
+  for (const addonId of data.addons) {
+    const element = document.querySelector(`[data-addon-id="${addonId}"]`);
+    const name = element.getAttribute("data-addon-name");
+    dataToCart.properties.set("Addon: " + name, true);
+  }
+  console.log(dataToCart);
+  try {
+    const response = await fetch("/cart/add.js", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        items: [dataToCart],
+      }),
+    });
+    if (response.status === 200) {
+      console.log("Success, added box to cart!");
+    } else {
+      console.error(`Error ${response.status}: ${response.statusText}`);
+    }
+  } catch (error) {
+    console.error("Error:", error);
+  }
+});
+
+// Checks whether the box is valid (cupcakes + addons) and then enables / disables
+// the add to cart button.
+function checkBoxIsValid() {
+  addToCartBtn.disabled = data.cupcakeLength() != data.maxCupcakes;
+}
+
+// ===================================
+// ON DOM LOAD
+// ===================================
 
 document.addEventListener("DOMContentLoaded", function () {
   readBoxLayout();
